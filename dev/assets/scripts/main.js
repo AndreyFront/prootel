7d326ee54661b63682d80694be9d696f23d9fc53
AndreyFront <andrey-se-v@yandex.ru>
2022-09-21 17:14:54 +0300

    made a desktop header and footer

--- dev/assets/scripts/main.js
+++ dev/assets/scripts/main.js
@@ -2,3 +2,17 @@
 // import IMask from 'imask'
 // import validator from 'validator'
 // https://addmorescripts.github.io/hystModal/index_ru.html
+
+window.onload = () => {
+    function header() {
+        const header = document.querySelector('[data-header="header"]')
+
+        if (!header) return
+
+        const heightHeader = header.offsetHeight
+        const page = document.querySelector('[data-page="page"]')
+        if (page) page.style.marginTop = `${heightHeader}px`
+    }
+
+    header()
+}
