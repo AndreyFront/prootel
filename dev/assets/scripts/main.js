3297e486973db388f093a68f9182553f0dc3736f
AndreyFront <andrey-se-v@yandex.ru>
2022-09-26 16:19:39 +0300

    half adapted header

--- dev/assets/scripts/main.js
+++ dev/assets/scripts/main.js
@@ -4,6 +4,51 @@
 // https://addmorescripts.github.io/hystModal/index_ru.html
 
 window.onload = () => {
+
+    function smoothView(btn, el, startHeight = 0) {
+
+        if (!btn && !el) return
+    
+        let heightEl = el.offsetHeight
+        el.classList.add('not-active')
+        el.style.height = `${startHeight}px`
+    
+        if (startHeight > 0) {
+            if (heightEl < startHeight) {
+                el.classList.remove('not-active')
+                el.style.height = `${heightEl}px`
+            }
+        }
+    
+        const update = () => {
+            el.style.height = 'auto'
+            setTimeout(() => {
+                heightEl = el.offsetHeight
+                el.style.height = `${heightEl}px`
+            }, 100)
+        }
+    
+        btn.addEventListener('click', () => {
+            if (el.classList.contains('not-active')) {
+                el.classList.remove('not-active')
+                el.style.height = `${heightEl}px`
+            } else {
+                el.classList.add('not-active')
+                el.style.height = `${startHeight}px`
+            }
+        })
+    
+        let observer = new MutationObserver(mutationRecords => {
+            update()
+        })
+          
+        observer.observe(el, {
+            childList: true, 
+            subtree: true,
+            characterDataOldValue: true
+        })
+    }
+
     function header() {
         const header = document.querySelector('[data-header="header"]')
 
@@ -11,7 +56,20 @@ window.onload = () => {
 
         const heightHeader = header.offsetHeight
         const page = document.querySelector('[data-page="page"]')
+
+        const phone = header.querySelector('[data-header="phone"]')
+
         if (page) page.style.marginTop = `${heightHeader}px`
+
+        if (window.matchMedia("(max-width: 1200px)").matches) {
+            const clonedNode = phone.cloneNode(true)
+            const nav = header.querySelector('[data-header="nav"]')
+
+            if (nav) {
+                nav.appendChild(clonedNode)
+                phone.remove()
+            }
+        }
     }
 
     function mainSlider() {
@@ -135,7 +193,42 @@ window.onload = () => {
         })
     }
 
+    function menu() {
+        const blockMenu = document.querySelector('[data-menu="block-menu"]')
+    
+        if (!blockMenu) return
+    
+        const content = blockMenu.querySelector('[data-menu="content"]')
+        const blockContent = document.querySelectorAll('[data-menu="block-content"]')
+
+        const btn = blockMenu.querySelector('[data-menu="btn"]')
+    
+        const header = document.querySelector('header')
+        const heightHeader = header.offsetHeight
+    
+        btn.addEventListener('click', (event) => {
+            event.preventDefault()
+            // content.style.top = `${heightHeader}px`
+            header.classList.toggle('active-menu')
+            document.body.classList.toggle('scroll-lock')
+        })
+
+        if (blockContent.length) {
+            blockContent.forEach(itemBlockContent => {
+                const header = itemBlockContent.querySelector('[data-menu="header"]')
+                const body = itemBlockContent.querySelector('[data-menu="body"]')
+    
+                smoothView(header, body)
+
+                header.addEventListener('click', () => {
+                    header.classList.toggle('active')
+                })
+            })
+        }
+    }
+
     header()
     mainSlider()
     reviews()
+    menu()
 }
